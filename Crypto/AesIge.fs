3f0b9eb5f6cce178dcb076c019dea995819e23ee
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-09-19 19:44:22 +0300

    Added an empty helper function `xorBlocks` to `AesIge.fs`.

--- Crypto/AesIge.fs
+++ Crypto/AesIge.fs
@@ -4,6 +4,9 @@ open System
 open System.Security.Cryptography
 
 module AesIge =
+    let private xorBlocks (src1: ReadOnlySpan<byte>) (src2: ReadOnlySpan<byte>) (dst: Span<byte>) (length: int) =
+        failwith "TODOO"
+
     let encrypt (data: byte[]) (key: byte[]) (iv: byte[]) : byte[] =
         failwith "TODOO"
 
