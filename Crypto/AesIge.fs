81db0d322903745a3acf4302450b3ad4683b4f5b
Qwexaro <SergeyIvanovWork47@gmail.com>
2026-09-19 19:33:11 +0300

    added empty function 'encrypt' to 'AesIge.fs'

---
+++ Crypto/AesIge.fs
@@ -0,0 +1,8 @@
+namespace Zephyr.Crypto
+
+open System
+open System.Security.Cryptography
+
+module AesIge =
+    let encrypt (data: byte[]) (key: byte[]) (iv: byte[]) : byte[] =
+        failwith "TODOO"
